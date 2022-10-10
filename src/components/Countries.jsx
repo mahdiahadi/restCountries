98989387ce2548d8c69742e918aa693646c13f3c
mehdiahadi <mahdiahadi23@gmail.com>
2022-10-10 11:03:34 +0330

    version 1.1 (edit filtering and searching)

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -6,27 +6,66 @@ import { Filter } from './Filter';
 import Loading from "./Loading"
 import Search from './Search';
 const Countries = () => {
-
+  const regions=[
+    {
+      name: "",
+      desc: "",
+    },
+    {
+      name: "africa",
+      desc: "africa",
+    },
+    {
+      name: "americas",
+      desc: "americas",
+    },
+    {
+      name: "asia",
+      desc: "asia",
+    },
+    {
+      name: "europe",
+      desc: "europe",
+    },
+    {
+      name: "oceania",
+      desc: "oceania",
+    },
+  ]
   const [countries, setCountries] = useState([]);
   const [loading,setLoading]=useState(false)
   const [searchTerm,setSearchTerm]=useState("")
   const [region, setRegion] = useState("")
   const { data:allCountries }=useGetCountriesQuery();
-  const { countryRegions }=useGetCountryRegionsQuery(region)
-    console.log(countries)
+  const { data:countryRegions }=useGetCountryRegionsQuery(region)
+
+    console.log(countryRegions)
 
     useEffect(()=>{
       setLoading(true);
-      let fetchData=allCountries?.filter((item) =>
-      Object.values(item)
-        .join("")
-        .toLowerCase()
-        .includes(searchTerm)
-    )
-
-      setCountries(fetchData)
+        if(searchTerm){
+          let fetchData=allCountries?.filter((item) =>
+          Object.values(item)
+            .join("")
+            .toLowerCase()
+            .includes(searchTerm)
+          )
+          setCountries(fetchData)
+        }else if(region !==""){
+          setCountries(countryRegions)
+        }else if(region===""){
+          let fetchData=allCountries?.filter((item) =>
+          Object.values(item)
+            .join("")
+            .toLowerCase()
+            .includes(searchTerm))
+            setCountries(fetchData)
+        }
+       
+                 
+      
       setLoading(false)
-    },[searchTerm,allCountries])
+    },[region,searchTerm,allCountries])
 
       // useEffect(() => {
       //   let filterData=countryRegions?.filter((item)=>
@@ -40,7 +79,7 @@ const Countries = () => {
     <>
       <div className="mt-10 flex align-items justify-between">
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-          <Filter region={region} setRegion={setRegion} />
+          <Filter regions={regions} setRegion={setRegion} />
      </div>
     
     <div className='mb-3 mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6'>
@@ -48,8 +87,8 @@ const Countries = () => {
         {
           countries?.map((country,index)=>(
              
-              <div className="flex align-items justify-center">
-                   <Country key={index} country={country} />
+              <div  key={index} className="flex align-items justify-center">
+                   <Country country={country} />
               </div>
           ))
         }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
-import { useGetCountriesQuery, useGetCountryRegionsQuery } from '../services/CountriesApi';
-export const Filter = ({setRegion}) => {
+import { Link } from 'react-router-dom'
+export const Filter = ({regions,setRegion}) => {
 
-  const { data }=useGetCountriesQuery();
  
   return (
     <div className="relative sm:ml-48 md:mr-72 sm:-mt-10 mt-3 mb-2">
@@ -10,10 +9,11 @@ export const Filter = ({setRegion}) => {
             placeholder='Select '
             className=" sm:w-72 w-72 h-10 dark:bg-gray-100 bg-gray-200  border rounded shadow-sm outline-none p-4 text-black hover:shadow-lg"
             onChange={(e)=>setRegion(e.target.value)}
+            value={regions.name}
             // filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
              >
             {
-              data?.map((item,index) => <option key={index} value={item.region} >{item.region}</option>)
+              regions?.map((item,index) => <option key={index} value={item.name} >{item.name}</option>)
             }
         </select>
     </div>
